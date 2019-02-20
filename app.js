6b9b0f0183810967dc90acbc72c571f2ad3b31f1
Mrlgm <877205619@qq.com>
2019-02-21 00:26:29 +0800

    修复了注册和上传图片冲突的bug

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const path = require('path')
 const notes = require('./routes/notes')
 const auth = require('./routes/auth')
 const index = require('./routes/index')
+const upload = require('./utils/upload')
 
 const app = new Koa()
 
@@ -56,6 +57,37 @@ app.use(static(path.join( __dirname,  staticPath)))
 // })
 
 //app.use(bodyParser())
+
+router.post('/', koaBody({
+  multipart:true,
+  encoding:'gzip',
+  formidable:{
+    uploadDir:path.join(__dirname,'/public/img'),
+    keepExtensions: true,
+    maxFieldsSize:200 * 1024 * 1024,
+    onFileBegin:(name,file) => {
+      // console.log(file);
+      // 获取文件后缀
+      const ext = upload.getUploadFileExt(file.name);
+      // 最终要保存到的文件夹目录
+      const dir = path.join(__dirname,'/public/img');
+
+      const fileName = upload.getUploadFileName(ext)
+      // 重新覆盖 file.path 属性
+      file.path = `${dir}/${fileName}`;
+      
+      app.context.uploadpath = app.context.uploadpath ? app.context.uploadpath : {};
+      app.context.uploadpath[name] = `img/${fileName}`;
+    },
+    onError:(err)=>{
+      console.log(err);
+    }
+  }
+}),async (ctx)=>{
+  // console.log(ctx.request.files);
+  console.log(ctx.uploadpath);
+  ctx.body = 'http://api.wwnight.cn/' + ctx.uploadpath.file
+});
 
 router.use('/', index.routes())
 router.use('/notes', notes.routes())
